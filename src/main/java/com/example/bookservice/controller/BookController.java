a4ae153d0450573bc6b819c495a8d656d7120869
EcCo18 <clemens.kurzeya@icloud.com>
2022-04-06 11:26:23 +0200

    fix(bookController): changed dockerfile, wrapped responses in ResponseEntities

--- src/main/java/com/example/bookservice/controller/BookController.java
+++ src/main/java/com/example/bookservice/controller/BookController.java
@@ -6,10 +6,12 @@ import com.example.bookservice.services.BookService;
 import com.example.bookservice.services.mapper.BookMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/book")
@@ -21,16 +23,23 @@ public class BookController {
     private final BookMapper bookMapper;
 
     @GetMapping()
-    public List<BookDto> getAllBooks() {
+    public ResponseEntity<List<BookDto>> getAllBooks() {
         log.info("received GET request for all books");
-        return bookMapper.mapBookListToDtoList(bookService.getAllBooks());
+        return ResponseEntity.ok(bookMapper.mapBookListToDtoList(bookService.getAllBooks()));
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<BookDto> getBookById(@PathVariable("bookId") int bookId) {
+        return bookService.findBook(bookId)
+                .map(book -> ResponseEntity.ok(bookMapper.mapBookToDto(book)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping()
-    public BookDto createBook(@Valid @RequestBody BookDto bookDto) {
+    public ResponseEntity<BookDto> createBook(@Valid @RequestBody BookDto bookDto) {
         log.info("received POST for book");
         log.debug(bookDto.toString());
         Book createdBook = bookService.createBook(bookMapper.mapDtoToBook(bookDto));
-        return bookMapper.mapBookToDto(createdBook);
+        return ResponseEntity.ok(bookMapper.mapBookToDto(createdBook));
     }
 }
--- src/main/java/com/example/bookservice/services/BookService.java
+++ src/main/java/com/example/bookservice/services/BookService.java
@@ -6,6 +6,8 @@ import com.example.bookservice.services.metrics.BookMetricService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
